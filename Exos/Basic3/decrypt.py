7a402a4cdf9242fab5fe104566d5543bb7692255
Pierre DALLARA <pierre1.dallara@epitech.eu>
2023-04-07 12:26:10 +0200

    [ADD] Find Basic3

---
+++ Exos/Basic3/decrypt.py
@@ -0,0 +1,27 @@
+import sys
+
+def xor_file(infile, outfile, key):
+    with open(infile, "rb") as f:
+        data = f.read()
+
+    # XOR operation
+    xor_data = bytearray()
+    for i in range(len(data)):
+        if (i < 40):
+            print(data[i] ^ key[i % len(key)])
+        xor_data.append(data[i] ^ key[i % len(key)])
+
+    # Write XOR data to output file
+    with open(outfile, "wb") as f:
+        f.write(xor_data)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python xor_bmp.py input_file output_file")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    key = bytes('gitgud'.encode())
+
+    xor_file(input_file, output_file, key)
