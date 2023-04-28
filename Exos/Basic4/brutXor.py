ce00bf5512c718e48bf039d9759dca0a98f2d0dd
Pierre <pierre1.dallara@epitech.eu>
2023-04-28 12:30:20 +0200

    [ADD] Basic4 / Try RSA2

--- Exos/Basic4/brutXor.py
+++ Exos/Basic4/brutXor.py
@@ -1,10 +1,14 @@
 tableauChiffre = bytes([0x25, 0x76, 0x30, 0x7B,
                         0x11, 0x91, 0x51, 0x61,
-                        0x07, 0x17, 0x70, 0x1E])
+                        0x07, 0x17, 0x70, 0x1E,
+                        0x3C, 0x00, 0x00
+                      ])
 
 tableauDechiffre = bytes([0x52, 0x49, 0x46, 0x46,   # RIFF
-                          0xE2, 0x04, 0x00, 0x00,   # 57956
-                          0x57, 0x45, 0x42, 0x50])  # WEBP
+                          0x5C, 0xE2, 0x00, 0x00,   # 57956 - 8
+                          0x57, 0x45, 0x42, 0x50,   # WEBP
+                          0x56, 0x50, 0x38,   # Magic Number
+                        ]) 
 resKeyTableau = []
 
 for i in range(len(tableauChiffre)):
@@ -13,3 +17,5 @@ for i in range(len(tableauChiffre)):
 
 key_bytes = bytes(resKeyTableau).hex()
 print(key_bytes)
+key_ascii = bytes.fromhex(key_bytes).decode('ascii')
+print(key_ascii)
--- Exos/Basic4/decryptWebp.py
+++ Exos/Basic4/decryptWebp.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
-    key = bytes([0x77, 0x3f, 0x76, 0x3d, 0xf3, 0x95, 0x51, 0x61, 0x50, 0x52, 0x32, 0x4e])
+    key = bytes('w?v=MsQaPR2NjP8'.encode())
 
     xor_file(input_file, output_file, key)
